c0c565e7e0079b269d275827a6ca1952634434f6
Zeeta:) <126560008+zzz3t444@users.noreply.github.com>
2024-09-26 11:58:18 +0700

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,18 @@ const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
+	build: {
+		chunkSizeWarningLimit: 1024,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return 'vendor'; // Put all dependencies in a 'vendor' chunk
+					}
+				},
+			},
+		},
+	},
 	site: process.env.CI
 		? 'https://themesberg.github.io'
 		: `http://localhost:${DEV_PORT}`,
